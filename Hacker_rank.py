69b3ed23d1d1dbe32dd6f84e5299f07cd5c3f638
Jivika Kaushik <kaushikjivika@gmail.com>
2026-01-20 21:17:10 +0530

    Added question 5 print an integer in the form of string and question 6 print all the coordinates.

--- Hacker_rank.py
+++ Hacker_rank.py
@@ -78,3 +78,53 @@ def is_leap(year):
         return leap
 year = int(input())
 
+""" Question 5:         Task : The included code stub will read an integer, n from STDIN Without using any string methods, try to print the following:   123....n
+                               
+                               Example :      n = 5 
+                                              Print the string 12345 """
+
+# solution:
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
+
+""" Question 6:         Task :   You are given three integers x,y, and z representing the dimensions of a cuboid along with an integer n.
+                                Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of i+j+k is not equal to n. 
+                                Here, 0<=i<=x; 0<=j<=y; 0<=k<=z
+                                
+                                Example : x=1, y=1, z=1, n=2
+                                          All permutations of (i,j,k) are:
+                                          (0,0,0)
+                                          (0,0,1)
+                                          (0,1,0)
+                                          (0,1,1)
+                                          (1,0,0)
+                                          (1,0,1)
+                                          (1,1,0)
+                                          (1,1,1)
+                                          Print an array of the elements that do not sum to n=2 :
+                                          [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]] ."""
+
+# solution:
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+result = [[i,j,k] 
+                  for i in range(x+1)
+                  for j in range(y+1)
+                  for k in range(z+1)
+                  if  i + j + k != n]
+
+print(result)
+
+
+
+
+
+
