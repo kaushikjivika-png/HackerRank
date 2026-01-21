6a7daf6574572664bf7a160f2e010344da13edaf
Jivika Kaushik <kaushikjivika@gmail.com>
2026-01-21 17:43:14 +0530

    Added question 7 to find runner -up score and question 8 to find average of score.

--- Hacker_rank.py
+++ Hacker_rank.py
@@ -123,8 +123,40 @@ result = [[i,j,k]
 
 print(result)
 
+""" Question 7:         Task :  Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. 
+                                Store them in a list and find the score of the runner-up. """
 
+# solution:
 
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    unique_score = set(arr) 
+    max_score = max(unique_score)
+    unique_score.remove(max_score) 
+    print(max(unique_score))    
+
+""" Question 8:         Task :  The provided code stub will read in a dictionary containing key/value pairs of name: [marks] for a list of students. 
+                                Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+                                Example:    marks key : value pairs are
+                                            'alpha' : [20,30,40]
+                                            'beta'  : [30,50,70]
+                                            query_name = 'beta'
+                                            The average of beta's marks is (30 + 50 + 70) / 3 = 50.00  """
+
+# solution:
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
 
 
 
