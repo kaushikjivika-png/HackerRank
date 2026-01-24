f4e32b2e5765103b3053119c12c89c5c29845621
Jivika Kaushik <kaushikjivika@gmail.com>
2026-01-24 22:16:47 +0530

    Added question 10 ,hash function.

--- Hacker_rank.py
+++ Hacker_rank.py
@@ -206,8 +206,22 @@ if __name__ == '__main__':
             print(lst)
     
 
+""" Question 10:         Task :  Given an integer,n and n space-separated integers as input, create a tuple,t of those n integers.
+                                 Then compute and print the result of hash(t). 
 
+                                 Input Format:
+                                 The first line contains an integer,n, denoting the number of elements in the tuple.
+                                 The second line contains n space-separated integers describing the elements in tuple t. """
+
+# solution:
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    t = tuple(integer_list)
+    print(hash(t))
    
+
 
 
 
