25121d70d18d484cbd2cba89e3561f716efc3a79
Jivika Kaushik <kaushikjivika@gmail.com>
2026-02-26 23:33:03 +0530

    added hackerrank numpy practice question

--- Hacker_rank_part2.py
+++ Hacker_rank_part2.py
@@ -33,4 +33,48 @@ res = numpy.array(arr)
 print(numpy.reshape(res,(3,3)))
 
 
-                        
+""" Question 3:     Task :  You are given two integer arrays of size NXP and MXP (N & M are rows, and P is the column). Your task is to concatenate the arrays along axis 0.
+
+                            Input Format : The first line contains space separated integers N, M and P.
+                                           The next  lines contains the space separated elements of the P columns.
+                                           After that, the next  lines contains the space separated elements of the P columns.
+                                           
+                            Sample Input :  4 3 2
+                                            1 2
+                                            1 2 
+                                            1 2
+                                            1 2
+                                            3 4
+                                            3 4
+                                            3 4
+                                             
+                            Sample Output : [[1 2]
+                                            [1 2]
+                                            [1 2]
+                                            [1 2]
+                                            [3 4]
+                                            [3 4]
+                                            [3 4]]  """
+
+# solution:
+
+import numpy
+
+N,M,P = map(int, input().split())
+arr1 = []
+for i in range(N):
+    arr1.append(list(map(int, input().split())))
+    
+arr2 = []
+for i in range(M):
+    arr2.append(list(map(int, input().split())))
+    
+arr1 = numpy.array(arr1)
+arr2 = numpy.array(arr2)
+
+result = numpy.concatenate((arr1, arr2), axis=0)
+print(result)
+
+
+
+
