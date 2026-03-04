4e4b49038bc9561e627284de518ea8460c1d2b1a
Jivika Kaushik <kaushikjivika@gmail.com>
2026-03-04 22:30:29 +0530

    Added numpy hackerrank practice question.

--- Hacker_rank_part2.py
+++ Hacker_rank_part2.py
@@ -136,6 +136,24 @@ print(numpy.transpose(arr))
 print(arr.flatten())
 
 
+""" Question 6:            Task : You are given a 1-D array A, . Your task is to print the floor,ceil and rint  of all the elements of A .
+
+                           Sample Input : 1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9
+                           Sample Output : [ 1.  2.  3.  4.  5.  6.  7.  8.  9.]
+                                           [  2.   3.   4.   5.   6.   7.   8.   9.  10.]
+                                           [  1.   2.   3.   4.   6.   7.   8.   9.  10.]"""
+
+# Solution:
+
+import numpy
+numpy.set_printoptions(legacy='1.13')
+
+A = numpy.array(list(map(float,input().split())))
+print(numpy.floor(A))
+print(numpy.ceil(A))
+print(numpy.rint(A))
+
+
 
 
 
