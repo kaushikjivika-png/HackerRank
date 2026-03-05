9da482f60758824a5502dd5260ad3fd0d1134d3b
Jivika Kaushik <kaushikjivika@gmail.com>
2026-03-05 23:55:22 +0530

    added hackerrank python practice question

--- hackerrank_basics_part1.py
+++ hackerrank_basics_part1.py
@@ -467,6 +467,45 @@ for i in range(N-2,0,-2):
     pattern = ".|."*i
     print(pattern.center(M,"-"))
     
+""" Question 18:            Task : Given an integer,n , print the following values for each integer i from 1 to n :
+                                1    Decimal
+                                2    Octal
+                                3    Hexadecimal (capitalized)
+                                4    Binary
+                                
+                            Sample Input : 17
+                            Sample Output : 1     1     1     1
+                                            2     2     2    10
+                                            3     3     3    11
+                                            4     4     4   100
+                                            5     5     5   101
+                                            6     6     6   110
+                                            7     7     7   111
+                                            8    10     8  1000
+                                            9    11     9  1001
+                                            10    12     A  1010
+                                            11    13     B  1011
+                                            12    14     C  1100
+                                            13    15     D  1101
+                                            14    16     E  1110
+                                            15    17     F  1111
+                                            16    20    10 10000
+                                            17    21    11 10001  """
+
+# solution:
+
+def print_formatted(number):
+    width = len(bin(number)[2:])
+    for i in range(1 , number+1):
+        d = str(i)
+        o = oct(i)[2:]
+        h = hex(i)[2:].upper()
+        b = bin(i)[2:]
+        print(d.rjust(width),o.rjust(width),h.rjust(width),b.rjust(width))    
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
 
 
 
